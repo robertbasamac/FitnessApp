7def0e339800f272d3f055b1d78797ecc9f30066
Robert Basamac <rbasamac@gmail.com>
2022-04-18 20:37:28 +0300

    Initial Commit

---
+++ FitnessApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FitnessApp
+//
+//  Created by Robert Basamac on 18.04.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
