0ea3d8c0dd5dd4c63e4a68a8b09fc520511b0902
Robert Basamac <rbasamac@gmail.com>
2023-08-28 15:41:17 +0300

    Add gitignore file and some method descriptions

--- FitnessApp/Helpers/View+Extensions.swift
+++ FitnessApp/Helpers/View+Extensions.swift
@@ -8,13 +8,28 @@
 import SwiftUI
 
 extension View {
-    /// Custom Spacers
+    /// Positions this view within an invisible frame with the width equal to `.infinity`.
+    ///
+    /// - Parameters:
+    ///   - alignment: The alignment of this view inside the resulting frame.
+    ///     Note that most alignment values have no apparent effect when the
+    ///     size of the frame happens to match that of this view.
+    ///
+    /// - Returns: A view with `.infinity` width.
     @ViewBuilder
     func hSpacing(_ alignment: Alignment) -> some View {
         self
             .frame(maxWidth: .infinity, alignment: alignment)
     }
     
+    /// Positions this view within an invisible frame with the height equal to `.infinity`.
+    ///
+    /// - Parameters:
+    ///   - alignment: The alignment of this view inside the resulting frame.
+    ///     Note that most alignment values have no apparent effect when the
+    ///     size of the frame happens to match that of this view.
+    ///
+    /// - Returns: A view with `.infinity` height.
     @ViewBuilder
     func vSpacing(_ alignment: Alignment) -> some View {
         self
